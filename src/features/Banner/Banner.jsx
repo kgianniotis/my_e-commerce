576d130e13acd0d994ea711f6051286906f87cd9
gianniotisk <ukostasu@gmail.com>
2026-02-05 19:19:09 +0200

    Add banner

---
+++ src/features/Banner/Banner.jsx
@@ -0,0 +1,18 @@
+import "./Banner.css";
+
+export default function Banner() {
+  return (
+    <div className="banner">
+      <div className="bannerTrack">
+        <span className="bannerText">
+          Free shipping on orders over €50 • New arrivals every week • Limited
+          offers •
+        </span>
+        <span className="bannerText">
+          Free shipping on orders over €50 • New arrivals every week • Limited
+          offers •
+        </span>
+      </div>
+    </div>
+  );
+}
